40b28c631be593900efe68b07d11bcce0532cfd0
FiraBro <jemalfiragos@gmail.com>
2026-02-15 09:36:28 +0300

    wrting comperhensive list

--- exercise.py
+++ exercise.py
@@ -5,7 +5,11 @@ sale_prices=[100, 120, 135, 150]
 
 quantities=[1000, 1500, 1300]
 
-for chair_type in products_on_sale:
-    for price in sale_prices:
-        for quantity in quantities:
-            print ([chair_type, price*quantity])
+# for chair_type in products_on_sale:
+#     for price in sale_prices:
+#         for quantity in quantities:
+#             print ([chair_type, price*quantity])
+
+
+comp_list = [price * quantity for price in sale_prices for quantity in quantities]
+print(comp_list)
